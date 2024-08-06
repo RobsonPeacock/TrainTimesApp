97f89456c0283dba1334ed2154a28abf585865c3
Robson Peacock <robson_peacock20@outlook.com>
2024-08-06 11:48:24 +0100

    Add arrivals controller and index view

---
+++ app/controllers/arrivals_controller.rb
@@ -0,0 +1,18 @@
+class ArrivalsController < ApplicationController
+  def index
+    arrivals_data = TflApiService.fetch_train_times
+
+    @arrivals = arrivals_data.map do |arrival_data|
+      arrival = Arrival.new(
+        scheduled_time: arrival_data['expectedArrival'],
+        actual_time: arrival_data['timeToStation'],
+        line: arrival_data['lineName'],
+        platform: arrival_data['platformName'],
+        direction: arrival_data['direction'],
+        destination: arrival_data['destinationName']
+      )
+
+      arrival.valid? ? arrival : {}
+    end
+  end
+end
