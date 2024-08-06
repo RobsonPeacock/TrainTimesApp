e773aeaa27ba232509dd63f3e83b9f6a8a14d7d6
Robson Peacock <robson_peacock20@outlook.com>
2024-08-06 11:46:28 +0100

    Update arrival model

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -6,8 +6,9 @@ class Arrival
   attr_accessor :scheduled_time, :actual_time, :line, :platform,
   :direction, :destination
 
-  validates_presence_of :line, :platform, :direction, :destination
-  validates :scheduled_time, :actual_time, format: { with: /\A\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z\z/ }
+  validates_presence_of :line, :platform
+  validates :scheduled_time, format: { with: /\A\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z\z/ }
+  validates :actual_time, numericality: { only_integer: true }
 
   def initialize(attributes = {})
     @scheduled_time = attributes[:scheduled_time]
